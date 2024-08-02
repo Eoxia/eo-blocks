a24904bc14b98af7e2419714d0ef9b78a25df2fa
eoxia-amandine <amandine@eoxia.com>
2024-08-02 17:18:06 +0200

    #10 [JS] Add utils JS function for API

--- blocks/build/kpi-contentment/index.asset.php
+++ blocks/build/kpi-contentment/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '54135be1069760bb3c59');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd147542f5806dc5a9530');
