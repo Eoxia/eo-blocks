0ac3cf44535950d4f9058c606f0fc5a19307da30
eoxia-amandine <amandine@eoxia.com>
2025-01-29 11:10:10 +0100

    #27 [Blocks] add: Carousel icons

--- blocks/build/eo-carousel-inner/index.asset.php
+++ blocks/build/eo-carousel-inner/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '4307801fff04b805b17b');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'bb4a59a60cc674fa628a');
--- blocks/build/eo-carousel/index.asset.php
+++ blocks/build/eo-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'fe50553395687b5d6f7e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'db1788678efcfda16278');
