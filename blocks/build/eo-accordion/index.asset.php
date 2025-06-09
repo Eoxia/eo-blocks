20a1de69f5bbee91ff62a9c6a88c268b7c1483d7
eoxia-amandine <amandine@eoxia.com>
2025-06-09 17:57:39 +0200

    #47 [Block] add: Accordion title custom size

--- blocks/build/eo-accordion/index.asset.php
+++ blocks/build/eo-accordion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '1d1369b6830b1c644345');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b28da91a2fcb7926693a');
