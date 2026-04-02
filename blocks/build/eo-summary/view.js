ca7b9631f442f4b2b8aad4ac1d5451045de3214a
eoxia-amandine <amandine@eoxia.com>
2026-04-02 14:54:24 +0200

    #65 [Block] add: Summary block mobile options

--- blocks/build/eo-summary/view.js
+++ blocks/build/eo-summary/view.js
@@ -16,27 +16,44 @@ var __webpack_exports__ = {};
    */
   var initializeBlock = function ($block) {
     $('.eo-summary__control').each(function () {
-      $($block).append('<a href="#' + $(this).attr('id') + '" class="eo-summary__item ' + $(this).attr('id') + '">' + $(this).attr('summary-label') + '</a>');
+      $($block).find('.eo-summary__container').append('<a href="#' + $(this).attr('id') + '" class="eo-summary__item ' + $(this).attr('id') + '">' + $(this).attr('summary-label') + '</a>');
     });
 
     // Smooth Scroll.
     $('.eo-summary__item').on('click', function () {
-      var offset = 20;
-      if ($('#wpadminbar').length > 0) {
-        offset += 32;
-      }
-      if ($('.site-header .header-sticky').length > 0) {
-        offset += $('.site-header .header-sticky').outerHeight();
-      } // Beflex theme fix.
+      scrollSummary.call(this);
+    });
 
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top - offset
-        }, 500);
+    // Menu.
+    $($block).find('.eo-summary__menu').on('click', function () {
+      $($block).toggleClass('is-active-mobile-menu');
+    });
+    $($block).find('.eo-summary__menu-close').on('click', function () {
+      $($block).removeClass('is-active-mobile-menu');
+    });
+    $($block).find('.eo-summary__item').on('click', function () {
+      if ($($block).hasClass('is-active-mobile-menu')) {
+        $($block).removeClass('is-active-mobile-menu');
+        scrollSummary.call(this);
       }
     });
+  };
+  var scrollSummary = function () {
+    var offset = 20;
+    if ($('#wpadminbar').length > 0) {
+      offset += 32;
+    }
+    if ($('.site-header .header-sticky').length > 0) {
+      offset += $('.site-header .header-sticky').outerHeight();
+    } // Beflex theme fix.
+
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - offset
+      }, 500);
+    }
   };
   var updateActiveMenu = function () {
     const menuItems = $('.eo-summary__item');
