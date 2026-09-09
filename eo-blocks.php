6c5c8df6ddecbf6763fbce9419cf43cbdf551f03
eoxia-amandine <amandine@eoxia.com>
2026-09-09 16:25:05 +0200

    Update plugin version

--- eo-blocks.php
+++ eo-blocks.php
@@ -3,8 +3,8 @@
  * Plugin Name:       EO Blocks
  * Description:       A collection of Gutenberg blocks for WordPress made by Eoxia
  * Requires at least: 7.0.0
- * Requires PHP:      7.0
- * Version:           2.0.0
+ * Requires PHP:      7.4
+ * Version:           2.1.0
  * Author:            Eoxia
  * Author URI:        https://www.eoxia.com
  * License:           GPL-3.0-or-later
